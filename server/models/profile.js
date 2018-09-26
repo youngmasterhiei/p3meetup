88b31bf0324b9fa9e5eed77fb2c10d2a82da3720
Jason Goeble <jason.goeble@gmail.com>
2018-09-26 14:05:20 -0400

    updated models

--- server/models/profile.js
+++ server/models/profile.js
@@ -1,22 +1,97 @@
-module.exports = function(sequelize, DataTypes) {
-    var Profile = sequelize.define("Profile", {
-      firstName: DataTypes.STRING,
-      lastName: DataTypes.STRING,
-      city: DataTypes.STRING,
-      state: DataTypes.STRING,
-      age: DataTypes.INTEGER,
-      gender: DataTypes.STRING,
-      gitHub: DataTypes.STRING,
-      linkedIn: DataTypes.STRING,
-
-
-      
+module.exports = function(sequelize, DataTypes) 
+{
+    const profile = sequelize.define("profile",
+    {
+      id:
+      {
+          //there may be an issue if the column is not CHAR (36)
+          //when testing need to see what datatype sequelize actually assigns
+          type: DataTypes.UUID,
+          primaryKey: true,
+          defaultValue: DataTypes.UUIDV4,
+          allowNull:false
+      },
+      user_id:
+      {
+        type: DataTypes.UUID,
+        allowNull: false
+      },
+      age:
+      {
+          type: DataTypes.INTEGER,
+          validate:
+          {
+              len: [1,2]
+          },
+          allowNull: true
+      },
+      gender:
+      {
+          type: DataTypes.ENUM,
+          values: ['Male', 'Female', 'Trans-gender', 'Non-Binary'],
+          allowNull: true
+      },
+      phone:
+      {
+          //would like front end validation for this value
+          //need to decide how to store, likely need to normalize
+          //thinking (xxx) xxx-xxxx
+          type: DataTypes.STRING,
+          validate:
+          {
+              len: [10,20]
+          },
+          allowNull: true
+      },
+      city:
+      {
+          type: DataTypes.UUID,
+          allowNull: true
+      },
+      state:
+      {
+          type: DataTypes.UUID,
+          allowNull: true
+      },
+      country:
+      {
+          type: DataTypes.UUID,
+          allowNull: true
+      },
+      github:
+      {
+          type: DataTypes.STRING,
+          allowNull: true
+      },
+      linkedin:
+      {
+          type: DataTypes.STRING,
+          allowNull: true
+      },
+      avatar:
+      {
+          type: DataTypes.BLOB,
+          allowNull: true
+      },
+      //need query place averaged value of entries from reputation into this field
+      rating:
+      {
+          type: DataTypes.DECIMAL(10,2),
+          allowNull: true
+      },
+      status:
+      {
+          type: DataTypes.ENUM,
+          allowNull: true,
+          values: ['Active', 'Inactive']
+      }
+    },
+    {
+      //this requires snake casing
+      underscored: true
     });
-    Profile.associate = function(models) {
-      Profile.belongsTo(models.User)
-    };
-    
-    return Profile;
+
+    return profile;
   };
   
 
